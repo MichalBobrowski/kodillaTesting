78bddfbee7e5790219999ca476e7efdb66c934ce
“Michal <bobrowski92@gmail.com>
2017-08-09 22:58:57 +0200

    second commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,9 +1,29 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
 
-    public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+    public static void main(String[] args) {
+
+        Calculator calculator1 = new Calculator();
+        int a = 10, b = 5;
+
+        if(calculator1.add(a , b) == a + b ){
+            System.out.println("Test ok");
+        }
+        else{
+            System.out.println("Error");
+        }
+
+        if(calculator1.subtract(a , b) == a - b ){
+            System.out.println("Test ok");
+        }
+        else{
+            System.out.println("Error");
+        }
+
     }
 
 }
