d6fcc5d6151f24cb67e2f4ae290e6e26c2f7a776
“Michal <bobrowski92@gmail.com>
2017-08-19 22:56:39 +0200

    Zadanie 7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,12 +1,13 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoeamBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.*;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
 public class StreamMain {
     public static void main(String[] args) {
-        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+        /*ExpressionExecutor expressionExecutor = new ExpressionExecutor();
 
         PoeamBeautifier poeamBeautifier = new PoeamBeautifier();
 
@@ -14,5 +15,9 @@ public class StreamMain {
         poeamBeautifier.beautify("Zdanie do upiększenia", (text -> text.toUpperCase()));
         poeamBeautifier.beautify("Zdanie do upiększenia", (text -> text.replace(" ", "KUM")));
         poeamBeautifier.beautify("Zdanie do upiększenia", (text -> text.replaceAll("Zdanie", "Wyrażenie")));
+        */
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
+
     }
 }
