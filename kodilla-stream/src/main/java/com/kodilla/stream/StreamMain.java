9aca533bd34be98c4c8aea857395eacbfd10f807
“Michal <bobrowski92@gmail.com>
2017-08-20 10:13:33 +0200

    Poprawiony main do 7.1 i 7.3

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 
+import com.kodilla.stream.beautifier.PoeamBeautifier;
 import com.kodilla.stream.forumuser.Forum;
 import com.kodilla.stream.forumuser.ForumUser;
 import java.time.LocalDate;
@@ -11,6 +12,16 @@ import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
+        //Zadanie 7.1
+        PoeamBeautifier poeamBeautifier = new PoeamBeautifier();
+
+        poeamBeautifier.beautify("Zdanie do upiększenia", (text -> "ABC " + text + " ABC"));
+        poeamBeautifier.beautify("Zdanie do upiększenia", (text -> text.toUpperCase()));
+        poeamBeautifier.beautify("Zdanie do upiększenia", (text -> text.replace(" ", "KUM")));
+        poeamBeautifier.beautify("Zdanie do upiększenia", (text -> text.replaceAll("Zdanie", "Wyrażenie")));
+
+
+        //Zadanie 7.3
         Map<Integer, ForumUser> resultMap0fForumUsers = Forum.getUsersList().stream()
                 .filter(user -> user.getSex() == 'M')
                 .filter(user -> user.getDateOfBirth().getYear() > 20)
