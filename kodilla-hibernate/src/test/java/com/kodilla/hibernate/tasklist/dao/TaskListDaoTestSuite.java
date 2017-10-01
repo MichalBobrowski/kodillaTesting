fce9dcce5754bfeb80b3703de9cf14f7cdc8c752
“Michal <bobrowski92@gmail.com>
2017-10-01 13:46:50 +0200

    Zadanie 13.2 dobre

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -11,7 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = TaskListDao.class)
+@SpringBootTest
 public class TaskListDaoTestSuite {
     @Autowired
     private TaskListDao taskListDao;
